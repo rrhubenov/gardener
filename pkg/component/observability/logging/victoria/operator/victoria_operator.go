c99a0edb5566c65dee294300b831d9f089c2b63d
Radoslav Hubenov <rrhubenov@gmail.com>
2025-10-15 16:24:06 +0300

    Include VPA to component

--- pkg/component/observability/logging/victoria/operator/victoria_operator.go
+++ pkg/component/observability/logging/victoria/operator/victoria_operator.go
@@ -10,10 +10,12 @@ import (
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -70,6 +72,7 @@ func (v *victoriaOperator) Deploy(ctx context.Context) error {
 	resources, err := registry.AddAllAndSerialize(
 		v.serviceAccount(),
 		v.deployment(),
+		v.vpa(),
 	)
 	if err != nil {
 		return err
@@ -192,6 +195,36 @@ func (v *victoriaOperator) deployment() *appsv1.Deployment {
 	}
 
 	return deployment
+}
+
+func (v *victoriaOperator) vpa() *vpaautoscalingv1.VerticalPodAutoscaler {
+	return &vpaautoscalingv1.VerticalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: v.namespace,
+			Labels:    GetLabels(),
+		},
+		Spec: vpaautoscalingv1.VerticalPodAutoscalerSpec{
+			TargetRef: &autoscalingv1.CrossVersionObjectReference{
+				APIVersion: appsv1.SchemeGroupVersion.String(),
+				Kind:       "Deployment",
+				Name:       deploymentName,
+			},
+			UpdatePolicy: &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeAuto),
+			},
+			ResourcePolicy: &vpaautoscalingv1.PodResourcePolicy{
+				ContainerPolicies: []vpaautoscalingv1.ContainerResourcePolicy{
+					{
+						ContainerName: vpaautoscalingv1.DefaultContainerResourcePolicy,
+						MinAllowed: corev1.ResourceList{
+							corev1.ResourceMemory: resource.MustParse("64Mi"),
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
 // GetLabels returns the labels for the victoria-operator.
